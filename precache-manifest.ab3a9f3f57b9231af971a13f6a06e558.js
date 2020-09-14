b65783ef1ac436f809f7b2cd7838afe82d534230
Ait0ne <bonafide112358@gmail.com>
2020-09-14 21:29:37 +0300

    Updates

---
+++ precache-manifest.ab3a9f3f57b9231af971a13f6a06e558.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae4bb4abd692727bf4b7a7500585f7a5",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "458c2616d99b3cec100c",
+    "url": "/Portfolio/static/css/2.ec700a2d.chunk.css"
+  },
+  {
+    "revision": "c4c165027c36df3f4c6a",
+    "url": "/Portfolio/static/css/main.16443a21.chunk.css"
+  },
+  {
+    "revision": "458c2616d99b3cec100c",
+    "url": "/Portfolio/static/js/2.d3edad06.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/Portfolio/static/js/2.d3edad06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4c165027c36df3f4c6a",
+    "url": "/Portfolio/static/js/main.5c77c40f.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  },
+  {
+    "revision": "8aed95c008aefe21a5f9e150684946c9",
+    "url": "/Portfolio/static/media/avatar.8aed95c0.jpg"
+  },
+  {
+    "revision": "3b68dcbe0a7a2f6bc9ca09a97509ba0c",
+    "url": "/Portfolio/static/media/bg.3b68dcbe.jpeg"
+  },
+  {
+    "revision": "2306e6ac9e5d6ac21e89ec7c51c3d5c2",
+    "url": "/Portfolio/static/media/funprazdnik-about.2306e6ac.png"
+  },
+  {
+    "revision": "6a759889685e8aa9cc3efbd50419e719",
+    "url": "/Portfolio/static/media/funprazdnik-ideas.6a759889.png"
+  },
+  {
+    "revision": "fd01947a8c7710555245a6f6d54cc8a1",
+    "url": "/Portfolio/static/media/funprazdnik-landingModal.fd01947a.png"
+  },
+  {
+    "revision": "7066e8ceb95c97df19577cd9699199ad",
+    "url": "/Portfolio/static/media/goals-main.7066e8ce.png"
+  },
+  {
+    "revision": "7f8015a34e9fe925c683ee99de5687ab",
+    "url": "/Portfolio/static/media/goals-mockup.7f8015a3.jpg"
+  },
+  {
+    "revision": "ee7bcc74dba2a970c00ba27b77386d3b",
+    "url": "/Portfolio/static/media/goals-tasks.ee7bcc74.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Portfolio/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Portfolio/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Portfolio/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Portfolio/static/media/slick.f97e3bbf.svg"
+  }
+]);
